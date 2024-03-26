63c91ad0e68e6b62c697d398b3c227d100605d10
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-03-26 18:16:09 +0530

    Basic Controls Demo

---
+++ 9-controls/basic-controls/src/main/java/lk/ijse/dep12/fx/controls/controller/MainViewController.java
@@ -0,0 +1,54 @@
+package lk.ijse.dep12.fx.controls.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class MainViewController {
+
+    public ToggleButton btnDisable;
+    public ToggleButton btnHide;
+    public CheckBox chkBackgroundColor;
+    public CheckBox chkTextFill;
+    public ColorPicker clrBackground;
+    public ColorPicker clrTextFill;
+    public Label lbl;
+    public Label lblOpacity;
+    public RadioButton rdOption1;
+    public RadioButton rdOption2;
+    public RadioButton rdOption3;
+    public Slider slrOpacity;
+    public TextField txt;
+
+    public void btnDisableOnAction(ActionEvent event) {
+
+    }
+
+    public void btnGetTextOnAction(ActionEvent event) {
+
+    }
+
+    public void btnHideOnAction(ActionEvent event) {
+
+    }
+
+    public void btnSetTextOnAction(ActionEvent event) {
+
+    }
+
+    public void chkBackgroundColorOnAction(ActionEvent event) {
+
+    }
+
+    public void chkTextFillOnAction(ActionEvent event) {
+
+    }
+
+    public void clrBackgroundOnAction(ActionEvent event) {
+
+    }
+
+    public void clrTextFillOnAction(ActionEvent event) {
+
+    }
+
+}
