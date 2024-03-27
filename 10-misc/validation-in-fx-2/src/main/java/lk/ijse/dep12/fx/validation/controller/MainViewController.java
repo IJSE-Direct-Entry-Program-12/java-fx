788392378d77f3f00634bb452e24b6477ddb8dfc
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-03-27 15:09:53 +0530

    Add Validation UI

---
+++ 10-misc/validation-in-fx-2/src/main/java/lk/ijse/dep12/fx/validation/controller/MainViewController.java
@@ -0,0 +1,36 @@
+package lk.ijse.dep12.fx.validation.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+public class MainViewController {
+
+    public Button btnNewProgram;
+    public Button btnSave;
+    public CheckBox chkGraduates;
+    public CheckBox chkProfessionals;
+    public CheckBox chkSchoolLeavers;
+    public CheckBox chkUndergraduates;
+    public ToggleGroup grpType;
+    public RadioButton rdFullTime;
+    public RadioButton rdPartTime;
+    public AnchorPane root;
+    public TextField txtCode;
+    public TextField txtId;
+    public TextField txtName;
+
+    public void btnNewProgramOnAction(ActionEvent event) {
+
+    }
+
+    public void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
